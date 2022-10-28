bb34ae01c12861e00a3ee915dc74fb0528b3b092
Robin Hafen <robin.hafen@teralytics.ch>
2022-10-28 15:02:06 +0200

    Set USE_TLS flag to true by default (#1434)

--- decentriq_platform/config.py
+++ decentriq_platform/config.py
@@ -4,7 +4,7 @@ DECENTRIQ_CLIENT_ID = os.getenv("DECENTRIQ_CLIENT_ID", "MHyVW112w7Ql95G96fn9rnLW
 
 DECENTRIQ_HOST = os.getenv("DECENTRIQ_HOST", "api-ch.decentriq.com")
 DECENTRIQ_PORT = int(os.getenv("DECENTRIQ_PORT", "443"))
-DECENTRIQ_USE_TLS = True if os.getenv("DECENTRIQ_USE_TLS", "true") == "true" else False
+DECENTRIQ_USE_TLS = False if os.getenv("DECENTRIQ_USE_TLS", "true") == "false" else True
 
 DECENTRIQ_API_CORE_HOST = os.getenv("DECENTRIQ_API_CORE_HOST", None)
 
@@ -14,7 +14,7 @@ else:
     DECENTRIQ_API_CORE_PORT = None
 
 if "DECENTRIQ_API_CORE_USE_TLS" in os.environ:
-    DECENTRIQ_API_CORE_USE_TLS = True if os.getenv("DECENTRIQ_API_CORE_USE_TLS") == "true" else False
+    DECENTRIQ_API_CORE_USE_TLS = False if os.getenv("DECENTRIQ_API_CORE_USE_TLS", "true") == "false" else True
 else:
     DECENTRIQ_API_CORE_USE_TLS = None
 
@@ -26,6 +26,6 @@ else:
     DECENTRIQ_API_PLATFORM_PORT = None
 
 if "DECENTRIQ_API_PLATFORM_USE_TLS" in os.environ:
-    DECENTRIQ_API_PLATFORM_USE_TLS = True if os.getenv("DECENTRIQ_API_PLATFORM_USE_TLS") == "true" else False
+    DECENTRIQ_API_PLATFORM_USE_TLS = False if os.getenv("DECENTRIQ_API_PLATFORM_USE_TLS", "true") == "false" else True
 else:
     DECENTRIQ_API_PLATFORM_USE_TLS = None
